a06759cfb5de06d067dc018164cba0c68025a3db
ljx <xianbu.ljx@alibaba-inc.com>
2025-09-12 17:32:21 +0800

    前端界面完成4个模块

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 750,
   });
 
   win.loadFile("./src/index.html");
